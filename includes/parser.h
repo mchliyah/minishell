8b7891d250753f17044e69c4201fe95e7378546b
Mohammed Chliyah <mchliyah@e1r12p6.1337.ma>
2022-06-19 20:44:35 +0100

    look at the previeuss commit

--- includes/parser.h
+++ includes/parser.h
@@ -6,30 +6,30 @@
 /*   By: mchliyah <mchliyah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 11:29:51 by mchliyah          #+#    #+#             */
-/*   Updated: 2022/06/17 17:04:40 by mchliyah         ###   ########.fr       */
+/*   Updated: 2022/06/19 20:31:33 by mchliyah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
 
- typedef struct s_pipeline
- {
-	 char				*type;
-	 struct s_pipeline	*left;
-	 struct s_pipeline	*right;
- } t_pipe_line;
+typedef struct s_cmd_word
+{
+	char	*type;
+	char	*arg;
+}	t_cmd_word;
 
- typedef struct s_cmd_word
- {
-	 char	*type;
-	 char	*arg;
- } t_cmd_word;
+typedef struct s_pipeline
+{
+	char				*type;
+	struct s_pipeline	*left;
+	struct s_pipeline	*right;
+}	t_pipe_line;
 
 typedef struct s_redirection
 {
-	char	*type;
-	struct s_redirection *next;
+	char					*type;
+	struct s_redirection	*next;
 }	t_redirection;
 
 //typedef struct s_files
