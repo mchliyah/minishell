dffa4392d9304378fe72bec7bcbb0b3336f7a866
Abdelhak El moussaoui <teqaniabde@gmail.com>
2022-08-14 15:43:02 +0100

    save

--- lexer/cmd_getter.c
+++ lexer/cmd_getter.c
@@ -12,6 +12,24 @@
 
 #include "../includes/minishell.h"
 
+//		c = (*this)->content[(*this)->i + 1];
+//		if ((*this)->c == SINGLE_QUOTE && (c == SPACE || c == EPIPE
+//										   || c == LESS || c == GREATER))
+//		{
+//			s = join_string(s, (*this)->c);
+//			break ;
+//		}
+//		else if ((*this)->c == SINGLE_QUOTE && (ft_isalnum(c) || c == SPACE))
+//		{
+//			while ((*this)->c)
+//			{
+//				if ((*this)->c == SPACE || (*this)->c == EPIPE
+//					|| (*this)->c == LESS || (*this)->c == GREATER)
+//					return (s);
+//				s = join_string(s, (*this)->c);
+//				*this = advance(*this);
+//			}
+//		}
 char	*get_s_quote(t_lexer **this)
 {
 	char	c;
@@ -24,24 +42,8 @@ char	*get_s_quote(t_lexer **this)
 	{
 		s = join_string(s, (*this)->c);
 		*this = advance(*this);
-		c = (*this)->content[(*this)->i + 1];
-		if ((*this)->c == SINGLE_QUOTE && (c == SPACE || c == EPIPE
-										   || c == LESS || c == GREATER))
-		{
-			s = join_string(s, (*this)->c);
+		if ((*this)->c == SINGLE_QUOTE)
 			break ;
-		}
-		else if ((*this)->c == SINGLE_QUOTE && (ft_isalnum(c) || c == SPACE))
-		{
-			while ((*this)->c)
-			{
-				if ((*this)->c == SPACE || (*this)->c == EPIPE
-					|| (*this)->c == LESS || (*this)->c == GREATER)
-					return (s);
-				s = join_string(s, (*this)->c);
-				*this = advance(*this);
-			}
-		}
 	}
 	return (s);
 }
@@ -71,46 +73,87 @@ char	*get_c_word(t_lexer **this)
 	}
 	return (s);
 }
+//  "''"'"$USER"'"''"
 
 char	*cmd_getter(t_lexer **lex)
 {
 	char	*ptr;
+	char	*s;
 
-	ptr = NULL;
-	if ((*lex)->c == R_DOUBLE_QUOTE)
+	ptr = ft_strdup("");
+	if (!ptr)
 	{
-		ptr = get_quote_things(lex);
-		while ((*lex)->content[(*lex)->i + 1] == L_DOUBLE_QUOTE)
-		{
-			*lex = advance(*lex);
-			ptr = ft_strjoin(ptr, get_quote_things(lex));
-		}
-		if (ft_isalnum((*lex)->content[(*lex)->i + 1]))
-		{
-			*lex = advance(*lex);
-			ptr = ft_strjoin(ptr, get_c_word(lex));
-		}
-		while ((*lex)->content[(*lex)->i + 1] == SINGLE_QUOTE)
-		{
-			*lex = advance(*lex);
-			ptr = ft_strjoin(ptr, get_s_quote(lex));
-		}
+		perror("malloc");
+		exit(1);
 	}
-	else if ((*lex)->c == SINGLE_QUOTE)
+	while ((*lex)->c == SPACE)
+		*lex = advance(*lex);
+	while ((*lex)->c != '\0')
 	{
-		ptr = get_s_quote(lex);
-		while ((*lex)->content[(*lex)->i + 1] == SINGLE_QUOTE)
-		{
+		if ((*lex)->c == L_DOUBLE_QUOTE)
+			s = get_quote_things(lex);
+		else if ((*lex)->c == SINGLE_QUOTE)
+			s = get_s_quote(lex);
+		else
+			s = get_c_word(lex);
+		printf("s %s\n", s);
+		ptr = ft_strjoin(ptr, s);
+		if ((*lex)->c != '\0')
 			*lex = advance(*lex);
-			ptr = ft_strjoin(ptr, get_quote_things(lex));
-		}
-		if (ft_isalnum((*lex)->content[(*lex)->i]))
-		{
-			*lex = advance(*lex);
-			ptr = ft_strjoin(ptr, get_c_word(lex));
-		}
+		if ((*lex)->c == EPIPE || (*lex)->c == LESS
+			||  (*lex)->c == GREATER || (*lex)->c == SPACE)
+			break ;
 	}
-	else
-		ptr = get_c_word(lex);
 	return (ptr);
 }
+
+//char	*cmd_getter(t_lexer **lex)
+//{
+//	char	*ptr;
+//
+//	ptr = NULL;
+//	if ((*lex)->c == R_DOUBLE_QUOTE)
+//	{
+//		ptr = get_quote_things(lex);
+//		if ((*lex)->c != '\0')
+//			*lex = advance(*lex);
+//		while ((*lex)->c == L_DOUBLE_QUOTE)
+//		{
+//			ptr = ft_strjoin(ptr, get_quote_things(lex));
+//			if ((*lex)->c != '\0')
+//				*lex = advance(*lex);
+//		}
+//		if (ft_isalnum((*lex)->c))
+//		{
+//			ptr = ft_strjoin(ptr, get_c_word(lex));
+//			//*lex = advance(*lex);
+//		}
+//		while ((*lex)->c == SINGLE_QUOTE)
+//		{
+//			ptr = ft_strjoin(ptr, get_s_quote(lex));
+//			if ((*lex)->c != '\0')
+//				*lex = advance(*lex);
+//		}
+//	}
+//	else if ((*lex)->c == SINGLE_QUOTE)
+//	{
+//		ptr = get_s_quote(lex);
+//		if ((*lex)->c != '\0')
+//			*lex = advance(*lex);
+//		while ((*lex)->c == SINGLE_QUOTE)
+//		{
+//			ptr = ft_strjoin(ptr, get_quote_things(lex));
+//			if ((*lex)->c != '\0')
+//				*lex = advance(*lex);
+//		}
+//		if (ft_isalnum((*lex)->c))
+//		{
+//			if ((*lex)->c != '\0')
+//				*lex = advance(*lex);
+//			ptr = ft_strjoin(ptr, get_c_word(lex));
+//		}
+//	}
+//	else
+//		ptr = get_c_word(lex);
+//	return (ptr);
+//}
